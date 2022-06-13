47b8295942522bba0ab4aa6c3f3ef17e4c065aec
taejun <ptj22024@gmail.com>
2022-06-13 17:38:40 +0900

    Dog 기능 완성!

--- src/com/kh/git/HelloGit.java
+++ src/com/kh/git/HelloGit.java
@@ -1,12 +1,16 @@
 package com.kh.git;
 
+import com.kh.git.animal.Dog;
+
 public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("HelloGit!!!");
-	
-	}
-	public void a() {
 		
-	};
+		//Dog 기능추가
+		Dog dog = new Dog();
+		dog.bark();
+
+	}
+	
 }
--- src/com/kh/git/animal/Dog.java
+++ src/com/kh/git/animal/Dog.java
@@ -1,5 +1,7 @@
 package com.kh.git.animal;
 
 public class Dog {
-
+	public void bark() {
+		System.out.println("멍멍!");
+	}
 }
